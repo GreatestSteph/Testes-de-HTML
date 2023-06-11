a0dbf6f09f768e15e952a5e4698fc9bd29b54d62
Stephanie Madia <102835302+GreatestSteph@users.noreply.github.com>
2023-06-11 05:46:22 -0300

    Add files via upload

---
+++ 92 Fundo que muda de cor js pt 2/teste5.js
@@ -0,0 +1,12 @@
+var cores = ["darkslategray", "darkslateblue", "cornflowerblue", "darkslateblue", "darkslategray"];
+// cria uma variável lista com as cores red blue green e red
+    var posição = 0;
+    // variavel é igual a 0
+    function mudaofundo() {
+      // função mudaofundo
+        document.body.style.backgroundColor = cores[posição];
+        posição = (posição + 1) % cores.length;
+        setTimeout(mudaofundo, 11000);
+    }
+
+    window.onload = mudaofundo;
