bb5b10d3d98e92143f7829e4242ee495875d0254
Stephanie Madia <102835302+GreatestSteph@users.noreply.github.com>
2023-06-11 23:49:35 -0300

    Add files via upload

---
+++ 93 Fundo que muda de cor js pt 3/README.md
@@ -0,0 +1,2 @@
+# Teste de fundo 3
+Aqui estou testando as cores do fundo com js
---
+++ 93 Fundo que muda de cor js pt 3/testecores1.js
@@ -0,0 +1,20 @@
+function m0ver(definacor)
+{
+    document.body.style.backgroundColor = 'red'
+    // muda a cor do fundo do documento para vermelho
+    document.body.style.color = "white"
+    // muda as letras do documento para branco
+    definacor.innerHTML='fundo vermelho e caracteres em branco'
+    // muda aquele textinho de passar o mouse para 'fundo vermelho e caracteres em branco'
+}
+
+function m0ut(definacor)
+{
+    document.body.style.backgroundColor = 'yellow'
+    // muda a cor do fundo do documento para amarelo
+    document.body.style.color = "blue"
+    // muda as letras do documento para azul
+    definacor.innerHTML='fundo amarelo e caracteres azuis'
+    // muda aquele textinho de passar o mouse para 'fundo amarelo e caracteres azuis'
+}
+
