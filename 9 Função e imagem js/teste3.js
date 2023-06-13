70d62a682e9dd81f92c8a7781ffbcc679529bcaf
Stephanie Madia <102835302+GreatestSteph@users.noreply.github.com>
2023-06-12 23:10:34 -0300

    Update teste3.js

--- 9 Função e imagem js/teste3.js
+++ 9 Função e imagem js/teste3.js
@@ -42,7 +42,6 @@ function sleep(delay) {
 // aqui fizemos uma imagem que muda repetidamente :D
 
 
-
 // agora vamos fazer outra coisa
 
 window.addEventListener('load', function() {
@@ -50,12 +49,34 @@ window.addEventListener('load', function() {
 // preciso pesquisar o que significa addEventListener
 // preciso pesquisar o que é load 
 // está e uma função
-    var imagem = document.getElementById('movendo');
+    var imagege = document.getElementById('movendo');
     // variável imagem guarda a id da imagem do gato
     var container = document.getElementById('corre');
     // variável container guarda a id da div corre
-    imagem.style.width = container.clientWidth;
-    // preciso pesquisar o que significa style.width e clientWidth
+    imagege.style.width = container.clientWidth;
+    // define que a imagem tem o mesmo width que o container
 });
 
 // observação: eu misturei diversos códigos meus e do chatgpt, funcionou de algum jeito lol.
+
+
+
+
+function teste(){
+  setInterval(mudaImg2, 1000)
+  // função aciona a outra função mudaImg2 e faz ela funcionar a cada 1 segundo
+}
+
+// outra função que muda a imagem repetidamente
+function mudaImg2(){
+  var imagigi = document.getElementById("image2").src;
+  // variavel imagigi pega o elemento que tem a id "image2", que está associado a src
+  // você pode usar alert(imagigi) para ver o local que está sendo usado
+  if (imagigi.indexOf("rindo.png") !=-1)
+  // se no local a imagem for diferente de -1 (se ela existir)
+    document.getElementById('image2').src = "triste.png";
+    // o documento retorna a imagem triste.png
+  else 
+    document.getElementById('image2').src = "rindo.png";
+    // retorna a imagem rindo novamente
+}
